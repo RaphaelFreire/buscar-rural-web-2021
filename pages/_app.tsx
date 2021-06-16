3224a66c5179d2032de865383f5231feeadaac95
Raphael Freire <raphaelmachadofreire@gmail.com>
2021-06-16 15:15:56 -0300

    theme colors defined

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
-import '../styles/globals.css'
+import '../src/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from '../src/styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   )
---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+import { extendTheme } from '@chakra-ui/react'
+const colors = {
+  brand: {
+    green: '#0A522F',
+    greenLight: '#276F4C',
+    white: '#FFFFFF',
+    brown: '#5D360C',
+    greenAccent: '#018A06',
+    black: '#333333',
+    gray: '#A3A3A3',
+    yellow: '#FFE37E',
+    grayLight: '#F6F6F6'
+  }
+}
+export const theme = extendTheme({ colors })
