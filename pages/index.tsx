98af8308dabc981a1516b1edc9ab2f99382db486
Raphael Freire <raphaelmachadofreire@gmail.com>
2021-06-17 09:57:34 -0300

    primary button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import { Box } from '@chakra-ui/react'
-import { ButtonPrimary } from '../components/Button/Button'
+import { ButtonPrimary } from '../components/Button/ButtonPrimary'
+
+import styles from '../styles/components/ButtonPrimary.module.css'
 
 export default function Home(): JSX.Element {
   return (
     <Box>
-      Elementos
-      <ButtonPrimary variant="outline">Raphael Freire</ButtonPrimary>
+      <ButtonPrimary className={styles.buttonPrimary}>
+        Cadastre-se agora
+      </ButtonPrimary>
     </Box>
   )
 }
